e7d853409a07f74cf553a4b900702001c9ab4d5d
Rodrick Sims <rsimstransport@gmail.com>
2021-10-02 13:06:04 -0400

    "added cloudinary foler and file"

--- cloudinary/cloudinary.js
+++ cloudinary/cloudinary.js
@@ -4,10 +4,3 @@ const multer = require("multer");
 const cloudinaryStorage = require("multer-storage-cloudinary");
 const dotenv = require("dotenv");
 dotenv.config();
-
-// load in cloudinary environment variables
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-});
