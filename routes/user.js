2735791d84eacce6ad6ad944536eb79181f24b65
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-10-02 14:05:00 -0400

    adding code

--- routes/user.js
+++ routes/user.js
@@ -133,3 +133,16 @@ router.post('/login', (req, res, next) => {
         res.send(userInfo);
     }
 );
+
+// post the route in order to log out
+router.post('/logout', (req, res) => {
+    if (req.user) {
+        req.logout()
+        res.send({ msg: 'logging out' })
+    } else {
+        res.send({ msg: 'no user to log out' })
+    }
+})
+
+
+module.exports = router
