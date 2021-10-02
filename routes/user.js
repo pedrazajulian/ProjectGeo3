f0f9d1167a2780fe932d9ad5b4cb13c422e4ce8c
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-10-02 13:58:34 -0400

    adding code

--- routes/user.js
+++ routes/user.js
@@ -9,3 +9,55 @@ router.get("/user", function (req, res) {
     db.Users.find({})
         .then((response) => res.json(response))
 });
+
+// posting route in order to create a new user
+router.post("/user", parser.single("image"), (req, res) => {
+    console.log("api POST request received");
+    console.log(req.file);
+    const newUser = {};
+    let image = {};
+
+    if (req.file) {
+        image.url = req.file.url;
+        image.id = req.file.public_id;
+    } else {
+        image = req.body.image
+    }
+
+    newUser.username = req.body.username;
+    newUser.firstname = req.body.firstname;
+    newUser.lastname = req.body.lastname;
+    newUser.password = req.body.password;
+    newUser.email = req.body.email;
+    newUser.image = image;
+
+    // checks if a username or email exists already, if it doesnt a new user is created
+    db.Users.findOne({ username: newUser.username }, (err, user) => {
+        if (err) {
+            console.log('User.js post error: ', err)
+        } else if (user) {
+            res.json({
+                error: `Sorry, already a user with the username: ${newUser.username}`
+            });
+        } else if (user) {
+            res.json({
+                error: `Sorry, already a user with the username: ${req.body.username}`
+            });
+        }
+        else {
+            db.Users.findOne({ email: newUser.email }, (err, user) => {
+                if (err) {
+                    console.log('User.js post error: ', err)
+                } else if (user) {
+                    res.json({
+                        error: `Sorry, already a user with the email: ${newUser.email}`
+                    })
+                } else {
+                    db.Users.create(newUser)
+                        .then((response) => res.json(response))
+                        .catch(err => res.status(422).json(err));
+                }
+            });
+        }
+    });
+});
