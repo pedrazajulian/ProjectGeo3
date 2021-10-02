2e8a0e6c5699b28264393ded764528a864eb2f31
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-10-02 13:33:56 -0400

    adding code to event.js

--- routes/event.js
+++ routes/event.js
@@ -4,3 +4,20 @@ const Moment = require("moment");
 var nodemailer = require('nodemailer');
 const parser = require("../cloudinary/cloudinary");
 const cloudinary = require("cloudinary");
+
+// Showing last 5 events on the home page route
+router.get("/event", function (req, res) {
+    db.Events.find({}).then((response) => {
+    
+
+        console.log(response);
+        console.log("showing events");
+        if (response.length > 5) {
+            let lastFive = response.slice(Math.max(response.length - 7, 1));
+            res.json(lastFive);
+        }
+        else {
+            res.json(response);
+        }
+    });
+});
