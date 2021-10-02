20d16f6eac54388111c004f16818e77ad2916a85
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-10-02 13:48:37 -0400

    adding code

--- routes/event.js
+++ routes/event.js
@@ -21,3 +21,56 @@ router.get("/event", function (req, res) {
         }
     });
 });
+
+// route for to get infp to display
+router.get("/event/:id", function (req, res) {
+    let id = req.params.id
+    db.Events.findById(id)
+        .populate("attendees")
+        .then((response) => {
+            let timeToEvent = Moment(`${response.date} ${response.time}`).fromNow();
+            res.json({ fromDB: response, time: timeToEvent });
+        });
+});
+
+
+// create new event
+router.post("/event", parser.single("image"), function (req, res) {
+    console.log("event POST request received");
+    let newEvent = {};
+    let image = {};
+
+    if (req.file) {
+        image.url = req.file.url;
+        image.id = req.file.public_id;
+    } else {
+        image = req.body.image;
+    }
+
+    newEvent.name = req.body.name;
+    newEvent.address = req.body.address;
+    newEvent.date = req.body.date;
+    newEvent.time = req.body.time;
+    newEvent.description = req.body.description
+    newEvent.organizer = req.body.organizer;
+    newEvent.image = image;
+
+    // takes the organizer's username to finds its objectId 
+    db.Users.findOne({ username: newEvent.organizer })
+        .then(response => newEvent.organizerId = response._id)
+        .then(response => {
+            // creates a new event and pushes its id to the user organizing
+            db.Events.create(newEvent)
+                .then((dbEvent) => {
+                    res.json(dbEvent);
+                    return db.Users.findByIdAndUpdate(
+                        newEvent.organizerId,
+                        { $push: { events: dbEvent._id } },
+                        { new: true }
+                    )
+                })
+                .catch(err => res.status(422).json(err))
+        })
+        .catch(err => console.log(err))
+
+});
