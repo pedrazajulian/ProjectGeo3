ca11e5cc3375b088c2eac86216bf39585f69e335
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-10-02 13:56:02 -0400

    adding code to user.js

--- routes/user.js
+++ routes/user.js
@@ -3,3 +3,9 @@ const db = require("../model");
 const passport = require("../passport");
 const parser = require("../cloudinary/cloudinary");
 const cloudinary = require("cloudinary");
+
+// getting routes for all users
+router.get("/user", function (req, res) {
+    db.Users.find({})
+        .then((response) => res.json(response))
+});
