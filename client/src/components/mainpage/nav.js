cd1130714db3a4890abd3e975545fe79bc5950af
DavidPedraza95 <82422713+DavidPedraza95@users.noreply.github.com>
2021-10-02 11:54:20 -0400

    adding files

---
+++ client/src/components/mainpage/nav.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./nav.css";
+
+function Nav(props) {
+  return (
+    <div class="navbar-fixed">
+      <nav>
+        <div className="nav-wrapper">
+          <a href="/" className="brand-logo left">
+            Crowfunding!
+          </a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            {/* Logging out sets state to Logged Out, and also directs you to home page */}
+            <li className={props.loggedIn === "true" ? "" : "hiddenForNav"}>
+              <Link to="#">{`Welcome, ${localStorage.getItem(
+                "username"
+              )}!`}</Link>
+            </li>
+            <li onClick={props.manageLogin} style={{ cursor: "pointer" }}>
+              <Link to={props.loggedIn === "true" ? "/" : "/loginpage"}>
+                {props.loggedIn === "true" ? "Log Out" : "Log In"}
+              </Link>
+            </li>
+            <li className={props.loggedIn === "true" ? "hiddenForNav" : ""}>
+              <Link to="/signup">Sign Up!</Link>
+            </li>
+            <li className={props.loggedIn === "true" ? "" : "hiddenForNav"}>
+              <Link to="/myevents">My Events</Link>
+            </li>
+            <li className={props.loggedIn === "true" ? "" : "hiddenForNav"}>
+              <Link to="/myaccount">My Account</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
