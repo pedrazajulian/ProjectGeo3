0211c6a43e5f13549fb2c2c183c66e14f7204279
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-10-02 14:02:19 -0400

    adding code

--- routes/user.js
+++ routes/user.js
@@ -69,3 +69,41 @@ router.get("/user/:id", (req, res) => {
         .then(user => res.json(user))
         .catch(err => res.json(err))
 });
+
+// route to update the user info
+router.put("/user/:id", parser.single("image"), (req, res) => {
+    db.Users.findById(req.params.id)
+        .then(user => {
+            const id = user.image.id;
+            let image = {};
+
+            // if uploads a new image, set image object properties to new image
+            if (req.file) {
+                console.log(req.file);
+                image.url = req.file.url;
+                image.id = req.file.public_id;
+                // takes old stored image id,then deletes it from cloudinary storage
+                if (id) {
+                    cloudinary.v2.uploader.destroy(id, (err, res) => {
+                        if(err) console.log(err);
+                        console.log("This is the response:" + res)
+                    });
+                }
+                // if user isnt uploading a new image then set the new image object to current image object
+            } else {
+                image = user.image;
+                console.log(image);
+            }
+            db.Users.findByIdAndUpdate(user._id, {
+                $set: {
+                    firstname: req.body.firstname,
+                    lastname: req.body.lastname,
+                    email: req.body.email,
+                    password: req.body.password,
+                    image: image
+                }
+            }, { new: true })
+                .then(updatedUser => res.json(updatedUser))
+        })
+        .catch(err => res.json(err));
+});
