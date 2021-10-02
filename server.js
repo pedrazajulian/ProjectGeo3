8aaeb2c5b6cf1790daad472326069c74e6fdebd6
julian <pedrazajulian@hotmail.com>
2021-10-02 13:31:03 -0400

    server update

---
+++ server.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const routes = require("./routes");
+const session = require("express-session");
+const passport = require("./passport");
+const MongoStore = require("connect-mongo")(session);
+const morgan = require("morgan");
+const PORT = process.env.PORT || 3001;
