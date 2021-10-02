a7f1bc7f464593152ac3c168e2696bc27245ee6c
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-10-02 14:04:19 -0400

    adding code

--- routes/user.js
+++ routes/user.js
@@ -115,3 +115,21 @@ router.get("/user/:id/myevents", (req, res) => {
         .then(user => res.json(user))
         .catch(err => res.json(err))
 });
+
+// post route to create a new login session
+router.post('/login', (req, res, next) => {
+    console.log('routes/user.js, login, req.body: ');
+    console.log(req.body)
+    next()
+},
+    passport.authenticate('local'),
+    (req, res) => {
+        console.log('logged in', req.user);
+        var userInfo = {
+            id: req.user._id,
+            username: req.user.username,
+            image: req.user.image.url
+        };
+        res.send(userInfo);
+    }
+);
