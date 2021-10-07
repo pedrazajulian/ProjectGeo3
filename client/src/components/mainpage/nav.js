38e653143960429942e60a3e8a210c98347fed2b
Andres Molestina <zocrates@gmail.com>
2021-10-07 19:43:18 -0400

    hamburger menu

--- client/src/components/mainpage/nav.js
+++ client/src/components/mainpage/nav.js
@@ -6,18 +6,12 @@ import "./nav.css";
 function Nav(props) {
   return (
     <div class="navbar-fixed">
-      <nav>
-        <div className="nav-wrapper">
-          <a href="/" className="brand-logo left">
-            Crowfunding!
-          </a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            {/* Logging out sets state to Logged Out, and also directs you to home page */}
-            <li className={props.loggedIn === "true" ? "" : "hiddenForNav"}>
-              <Link to="#">{`Welcome, ${localStorage.getItem(
-                "username"
-              )}!`}</Link>
-            </li>
+
+<nav>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo">Logo</a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
             <li onClick={props.manageLogin} style={{ cursor: "pointer" }}>
               <Link to={props.loggedIn === "true" ? "/" : "/loginpage"}>
                 {props.loggedIn === "true" ? "Log Out" : "Log In"}
@@ -31,10 +25,26 @@ function Nav(props) {
             </li>
             <li className={props.loggedIn === "true" ? "" : "hiddenForNav"}>
               <Link to="/myaccount">My Account</Link>
+      </ul>
+    </div>
+  </nav>
+
+  <ul class="sidenav" id="mobile-demo">
+            <li onClick={props.manageLogin} style={{ cursor: "pointer" }}>
+              <Link to={props.loggedIn === "true" ? "/" : "/loginpage"}>
+                {props.loggedIn === "true" ? "Log Out" : "Log In"}
+              </Link>
             </li>
-          </ul>
-        </div>
-      </nav>
+            <li className={props.loggedIn === "true" ? "hiddenForNav" : ""}>
+              <Link to="/signup">Sign Up!</Link>
+            </li>
+            <li className={props.loggedIn === "true" ? "" : "hiddenForNav"}>
+              <Link to="/myevents">My Events</Link>
+            </li>
+            <li className={props.loggedIn === "true" ? "" : "hiddenForNav"}>
+              <Link to="/myaccount">My Account</Link>
+  </ul>
+          
     </div>
   );
 }
